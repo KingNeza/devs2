f4689a6cc3e8aa5aeabea304cfaa404a4d736fcb
Mi ordenador <miordenador@MacBook-Air-de-Mi.local>
2022-07-22 14:27:32 -0500

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x0B4284e963730201e2e6Ae0B35199AE0F1BE0Eca";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
